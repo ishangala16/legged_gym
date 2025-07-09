0f977f3ec4f6024d0d9805cdcdc7d93da66a0c8e
Ishan Gala <ishangala16@gmail.com>
2025-07-09 10:01:34 -0700

    Add go2w environment and updates to math utilities

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -38,6 +38,8 @@ from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
+from .go2w.go2w_robot import Go2w
+from legged_gym.envs.go2w.go2w_config import GO2WRoughCfg, GO2WRoughCfgPPO
 
 
 import os
@@ -49,3 +51,4 @@ task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCf
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+task_registry.register( "go2w", Go2w, GO2WRoughCfg(), GO2WRoughCfgPPO())
